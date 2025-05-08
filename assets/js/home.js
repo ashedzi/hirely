687efbf7bad3c45636224f0d37aee92b2112815f
Ayomide Boye <boyeayomide@gmail.com>
2025-05-08 11:08:28 -0500

    linked fetch api data for random users from JS to HTML

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,8 +3,8 @@
 const URL = "https://randomuser.me/api/?nat=CA&results=10&seed=same";
 
 const options = {
-    methods: "GET",
-    header: {
+    method: "GET",
+    headers: {
         'Content-Type': 'application/JSON; charset=UTF-8'
     },
     mode: 'cors'
@@ -18,9 +18,26 @@ async function getUser(endpoint) {
         }
 
         const data = await result.json();
-        console.log(data.results);
+        displayUser(data.results);
 
     } catch (error) {
         console.error(error.message);
     }
 }
+
+function displayUser(users) {
+    const thirdContainer = document.querySelector('.third-column');
+    
+    users.forEach(user => {
+        const userCard = document.createElement('div');
+        userCard.classList.add('user-card');
+        userCard.innerHTML = `
+            <img src="${user.picture.small}" alt="User Picture">
+            <h3>${user.name.first} ${user.name.last}</h3>
+            <p>${user.occupation}</p>
+        `;
+        thirdContainer.appendChild(userCard);
+    });
+}
+
+getUser(URL);
