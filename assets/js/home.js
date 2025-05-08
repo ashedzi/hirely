a5f37304d8622873032754678c9c85ebea9bdfdd
Ayomide Boye <boyeayomide@gmail.com>
2025-05-08 10:42:34 -0500

    Added fetch api for random users

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const URL = "https://randomuser.me/api/?nat=CA&results=10&seed=same";
+
+const options = {
+    methods: "GET",
+    header: {
+        'Content-Type': 'application/JSON; charset=UTF-8'
+    },
+    mode: 'cors'
+}
+
+async function getUser(endpoint) {
+    try {
+        const result = await fetch(endpoint, options);
+        if(!result.ok) {
+            throw new error(`${result.status}: ${result.statusText}`);
+        }
+
+        const data = await result.json();
+        console.log(data.results);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
